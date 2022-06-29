913d9fda2c8117e67b4d3d3c08afd0cc64893a66
Manasamajoju <manasa.majoju@axiomio.com>
2022-06-29 10:41:03 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Post from "./Post"; 
+import Theme from "./Theme";
 
-function App() {
+import { BrowserRouter as Router, Routes } from 'react-router-dom';
+
+const Route = require('react-router-dom').Route;
+ function App() {
+  const myStyle = {
+    backgroundImage:
+      "url('https://images.unsplash.com/photo-1601674957874-1284f5996fea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE1fHx8ZW58MHx8fHw%3D&&q=80')",
+    height: '1000px',
+    width: '500',
+    marginTop: '-100px',
+    fontSize: '20px',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={myStyle}>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<Theme />} />
+          <Route path="Post/:postId" element={<Post />} />
+          
+        </Routes>
+      </Router>
+      </div>
     </div>
+  
   );
 }
 
---
+++ src/Theme.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import './header.css';
+
+import { Container, Row, Col, Button } from 'react-bootstrap';
+
+function Theme() {
+  const [results, setResult] = useState([]);
+  useEffect(() => {
+    callApi();
+  }, []);
+
+  function callApi() {
+    console.log('hello');
+    let url = 'https://gorest.co.in/public/v2/posts';
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-TYpe': 'application/json',
+      },
+    }).then((result) => {
+      result.json().then((resp) => {
+        console.log('results', resp);
+        setResult(resp);
+      });
+    });
+  }
+
+  return (
+    <div className="Theme">
+      <Container className="container">
+        <Row>
+          <Col className="col" sm={3}>
+            <div className="headingTitle">All Users</div>
+            <div className="">
+              <Button variant="outline-primary">
+                <a href="/User">User-ID</a>
+              </Button>{' '}
+            </div>
+          </Col>
+
+          <Col className="col1" sm={9}>
+            <div className="headingTitle">All Posts</div>
+
+            <div className="button">
+              {results?.map((row, index) => {
+                return (
+                  <Button
+                    className="buttonsCard"
+                    key={index}
+                    variant="outline-primary"
+                  >
+                    <a href={`/post/${row.id}`}>Post Id: {row.id}</a>
+                    &nbsp;&nbsp;
+                  </Button>
+                );
+              })}
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default Theme;
